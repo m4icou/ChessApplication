4486e23d910251def1a36e6b4f7e0e85e85990ee
Michael Douglas <michaeldouglas.santosdesouza@gmail.com>
2021-02-25 21:52:41 -0300

    Tabuleiro Exception

--- Tabuleiro/TabuleiroClass.cs
+++ Tabuleiro/TabuleiroClass.cs
@@ -15,14 +15,40 @@ namespace Tabuleiro
             Pecas = new Peca[linha, coluna];
         }
 
+        public bool ExistePeca(Posicao posicao){
+            VaidarPosicao(posicao);
+            return peca(posicao) != null;
+        }
+
         public Peca peca(int linha, int coluna)
         {
             return Pecas[linha, coluna];
         }
 
-        public void ColocarPeca(Peca p, Posicao pos){
+        public Peca peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha > Linha || pos.Coluna < 0 || pos.Coluna > Coluna)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void VaidarPosicao(Posicao posicao){
+            if(!PosicaoValida(posicao)){
+                throw new TabuleiroException ("Posição invalida");
+            }
         }
     }
 }
---
+++ Tabuleiro/TabuleiroException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+        
+    }
+}
